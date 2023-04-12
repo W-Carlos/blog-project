65e14468ac66bae54e000e492c1598b3278bf95c
W-Carlos <wendcarlos18@hotmail.com>
2023-04-12 14:06:32 -0300

    feat: I made the page that shows the users

--- src/containers/Users/index.jsx
+++ src/containers/Users/index.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../../axios/config";
 import './style.css'
 
-function Users(){
-    return(
-        <div>
-            <h2>Usuarios</h2>
+function Users() {
+    const [users, setUsers] = useState([])
+
+    const fetchApi = async () => {
+        const { data } = await api.get('/users')
+
+        console.log(data)
+        setUsers(data)
+    }
+
+    useEffect(() => {
+        fetchApi()
+    }, [])
+
+    return (
+        <div className="users">
+            <h2>Todos os Usuários</h2>
+            <ul className="list-users">
+                {
+                    users.map((user) => (
+                        <li key={user.id} className="user-container">
+                            <p>
+                                <b>Nome: </b> 
+                                {user.name}
+                            </p>
+                            <p>
+                                <b>Usuário: </b>
+                                {user.username}
+                            </p>
+                            <p>
+                                <b>Email: </b>
+                                {user.email}
+                            </p>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
